9aec11067d57557672f8d08fd4f8daa67a9e355c
yaoxiangshared <1710683889@qq.com>
2018-01-07 23:48:54 +0800

    添加小程序用户model

---
+++ app/MicroappUser.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class MicroappUser extends Model
+{
+    //
+    protected $table = 'user';
+
+    public $timestamps = false;
+
+}
